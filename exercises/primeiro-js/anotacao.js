7abce55a32a746be8284939ca43cf467832e4c6c
Marcelo Duarte <Contato.marccelo125@gmail.com>
2023-10-18 17:30:02 -0300

    [new] transferindo para js de anotações

---
+++ exercises/primeiro-js/anotacao.js
@@ -0,0 +1,31 @@
+alert("Olá Mundo!")
+
+// string
+let nome = "Marcelo"
+
+// number
+let idade = 19
+let peso = 59.2
+
+// boolean
+let estaChovendo = true
+let parouDeChover = false
+
+// variavel vazia
+let sobrenome
+
+// preenchendo variavel vazia
+sobrenome = "Duarte"
+
+// imprimir mensagem
+console.log(`Olá, meu nome é ${nome} e minha idade é ${idade} anos.`)
+
+// EXEMPLO DE CÓDIGO
+const chover = true
+
+if (chover) {
+    console.log("Não vou ao parque")
+}
+else {
+    console.log("Não vou ao parque")
+}
