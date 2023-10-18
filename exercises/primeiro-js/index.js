989621788e2ea82fa3cee096d2e8cb4a68f5e304
Marcelo Duarte <Contato.marccelo125@gmail.com>
2023-10-18 15:34:05 -0300

    [update] conteúdo da aula

--- exercises/primeiro-js/index.js
+++ exercises/primeiro-js/index.js
@@ -1,1 +1,21 @@
 alert("Olá Mundo!")
+
+// string
+let nome = "Marcelo"
+
+// number
+let idade = 19
+let peso = 59.2
+
+// boolean
+let estaChovendo = true
+let parouDeChover = false
+
+// variavel vazia
+let sobrenome
+
+// preenchendo variavel vazia
+sobrenome = "Duarte"
+
+// imprimir mensagem
+console.log(`Olá, meu nome é ${nome} e minha idade é ${idade} anos.`)
