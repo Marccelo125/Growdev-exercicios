a9f862aee7419ffd88f691b354553c23f90865d4
Marcelo Duarte <Contato.marccelo125@gmail.com>
2023-11-07 15:14:11 -0300

    🚧 update: array & objetos

--- exercises/exercises-js/anotacao.js
+++ exercises/exercises-js/anotacao.js
@@ -1,53 +1,53 @@
-// alert("Olá Mundo!")
+alert("Olá Mundo!")
 
-// // string
-// let nome = "Marcelo"
+// string
+let nome = "Marcelo"
 
-// // number
-// let idade = 19
-// let peso = 59.2
+// number
+let idade = 19
+let peso = 59.2
 
-// // boolean
-// let estaChovendo = true
-// let parouDeChover = false
+// boolean
+let estaChovendo = true
+let parouDeChover = false
 
-// // variavel vazia
-// let sobrenome
+// variavel vazia
+let sobrenome
 
-// // preenchendo variavel vazia
-// sobrenome = "Duarte"
+// preenchendo variavel vazia
+sobrenome = "Duarte"
 
-// // imprimir mensagem
-// console.log(`Olá, meu nome é ${nome} e minha idade é ${idade} anos.`)
+// imprimir mensagem
+console.log(`Olá, meu nome é ${nome} e minha idade é ${idade} anos.`)
 
-// // EXEMPLO DE CÓDIGO
-// const chover = true
+// EXEMPLO DE CÓDIGO
+const chover = true
 
-// if (chover) {
-//     console.log("Não vou ao parque")
-// }
-// else {
-//     console.log("Não vou ao parque")
-// }
+if (chover) {
+    console.log("Não vou ao parque")
+}
+else {
+    console.log("Não vou ao parque")
+}
 
-// // um array basicamente é um objeto que suas propriedades são números
-// const array = ['1', '2', '3', '4', '5'] 
-// // outro jeito de ver um array
-// const arrayOutro = {
-//     0: 1,
-//     1: 2,
-//     2: 3,
-//     3: 4,
-//     4: 5
-// }
+// um array basicamente é um objeto que suas propriedades são números
+const array = ['1', '2', '3', '4', '5'] 
+// outro jeito de ver um array
+const arrayOutro = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4,
+    4: 5
+}
 
-// // Objeto comum
-// const marceloDuarte = {
-//     nome: 'Marcelo Duarte',
-//     idade: 19,
-//     telefone: '(51) 9 xxxx-xxxx',
-//     cpf: 'xxx.xxx.xxx-xx',
-// }
+// Objeto comum
+const marceloDuarte = {
+    nome: 'Marcelo Duarte',
+    idade: 19,
+    telefone: '(51) 9 xxxx-xxxx',
+    cpf: 'xxx.xxx.xxx-xx',
+}
 
 // Objeto dentro de objeto e array
 const treinador = {
