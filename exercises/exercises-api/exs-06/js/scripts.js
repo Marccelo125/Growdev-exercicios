42cfdc12daa3b8b9397f34361156eeaf4ff50ef1
Marcelo Duarte <Contato.marccelo125@gmail.com>
2023-12-14 18:04:05 -0300

    :construction: update: exs 06 api [pagination]

--- exercises/exercises-api/exs-06/js/scripts.js
+++ exercises/exercises-api/exs-06/js/scripts.js
@@ -1,39 +1,45 @@
-const messagesContainer = document.querySelector('.messages-list')
+const messagesContainer = document.querySelector(".messages-list");
+const createMessageButton = document.querySelector("#newMessageButton");
 
-const prevPage = document.getElementById('prevPage')
-const nextPage = document.getElementById('nextPage')
+const prevPage = document.getElementById("prevPage");
+const nextPage = document.getElementById("nextPage");
 
 // Variáveis globais
-let currentPage = 1
-let totalPages = 1
+let currentPage = 1;
+let totalPages = 1;
+
+createMessageButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  location.href = "./cadastrar-recado.html";
+});
 
 async function fetchMessages(page) {
   try {
-    const userId = localStorage.getItem('userId')
+    const userId = localStorage.getItem("userId");
 
     if (!userId) {
-      alert("Você precisa fazer login para visualizar os recados.")
-
-      return
+      alert("Você precisa fazer login para visualizar os recados.");
+      location.href = "index.html";
+      return;
     }
 
     const params = {
       page,
-      perPage: 3
-    }
+      perPage: 3,
+    };
 
-    const response = await api.get(`/notes/${userId}`, { params })
-    const messages = response.data.userMessages
+    const response = await api.get(`/notes/${userId}`, { params });
+    const messages = response.data.userMessages;
 
-    console.log(messages)
+    console.log(messages);
 
-    totalPages = response.data.totalPages
+    totalPages = response.data.totalPages;
 
-    messagesContainer.innerHTML = ''
+    messagesContainer.innerHTML = "";
 
-    messages.forEach(message => {
-      const messageCard = document.createElement('div')
-      messageCard.classList.add('card')
+    messages.forEach((message) => {
+      const messageCard = document.createElement("div");
+      messageCard.classList.add("card");
 
       messageCard.innerHTML = `
         <h2 class="card-title">${message.title}</h2>
@@ -42,52 +48,63 @@ async function fetchMessages(page) {
           <i class="fas fa-solid fa-trash" data-id=${message.id}></i>
           <i class="fas fa-regular fa-edit" data-id=${message.id}></i>
         </div>
-      `
+      `;
 
-      messagesContainer.appendChild(messageCard)
+      messagesContainer.appendChild(messageCard);
 
-      const deleteIcon = messageCard.querySelector('.fa-trash')
+      const deleteIcon = messageCard.querySelector(".fa-trash");
 
-      deleteIcon.addEventListener('click', () => {
-        const messageId = deleteIcon.getAttribute('data-id')
+      deleteIcon.addEventListener("click", () => {
+        const messageId = deleteIcon.getAttribute("data-id");
 
-        deleteMessage(messageId)
-      })
+        deleteMessage(messageId);
+      });
 
-      const editIcon = messageCard.querySelector('.fa-edit')
-      editIcon.addEventListener('click', () => {
-        const messageId = editIcon.getAttribute('data-id')
+      const editIcon = messageCard.querySelector(".fa-edit");
+      editIcon.addEventListener("click", () => {
+        const messageId = editIcon.getAttribute("data-id");
 
-        navigateToEditPage(messageId)
-      })
+        navigateToEditPage(messageId);
+      });
     });
 
     if (messages.length === 0) {
-      const h3 = document.createElement('h3')
-      h3.textContent = 'Nenhum recado cadastrado.'
-      messagesContainer.appendChild(h3)
+      const h3 = document.createElement("h3");
+      h3.textContent = "Nenhum recado cadastrado.";
+      messagesContainer.appendChild(h3);
     }
   } catch (error) {
-    console.log('Erro ao buscar mensagens', error)
+    console.log("Erro ao buscar mensagens", error);
   }
 }
-
-fetchMessages(currentPage)
 
 function navigateToEditPage(messageId) {
-  location.href = `editar-recado.html?id=${messageId}`
+  location.href = `editar-recado.html?id=${messageId}`;
 }
 
-prevPage.addEventListener('click', () => {
+prevPage.addEventListener("click", () => {
   if (currentPage > 1) {
-    currentPage--
-    fetchMessages(currentPage)
+    currentPage--;
+    fetchMessages(currentPage);
   }
-})
+});
 
-nextPage.addEventListener('click', () => {
+nextPage.addEventListener("click", () => {
   if (currentPage < totalPages) {
     currentPage++;
-    fetchMessages(currentPage)
+    fetchMessages(currentPage);
   }
-})
+});
+
+async function numberPage(page) {
+  await fetchMessages(currentPage);
+  const pageNumbers = document.getElementById("pageNumbers");
+
+  if (!pageNumbers.children.length) {
+    for (let contador = 1; contador <= totalPages; contador++) {
+      pageNumbers.innerHTML += `<button onclick=fetchMessages(${contador}) >${contador}</button>`;
+    }
+  }
+}
+
+numberPage(currentPage);
