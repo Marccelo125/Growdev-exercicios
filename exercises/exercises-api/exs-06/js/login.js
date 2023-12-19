142a3c6421e12383a74946e6e43a96b99b3f1a9d
Marcelo Duarte <Contato.marccelo125@gmail.com>
2023-12-19 15:29:29 -0300

    :construction: update: exs 06 api [check email & password]

--- exercises/exercises-api/exs-06/js/login.js
+++ exercises/exercises-api/exs-06/js/login.js
@@ -1,31 +1,57 @@
-const formLogin = document.getElementById('form-login')
+const formLogin = document.getElementById("form-login");
 
-const emailInput = document.getElementById('email-login')
-const passwordInput = document.getElementById('password-login')
+const emailInput = document.getElementById("email-login");
+const passwordInput = document.getElementById("password-login");
 
-formLogin.addEventListener('submit', (e) => {
-  e.preventDefault() // impedir comportamento padrão do submit
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault(); // impedir comportamento padrão do submit
 
   const data = {
     email: emailInput.value,
     password: passwordInput.value,
-  }
+  };
 
-  login(data)
-})
+  if (!emailInput.value) {
+    setError(emailInput, "E-mail é obrigatório.");
+  } else {
+    setSuccess(emailInput);
+  } 
+
+  if (!passwordInput.value) setError(passwordInput, "Senha é obrigatória.");
+  else if (passwordInput.value.length < 6)
+    setError(passwordInput, "Senha precisa conter no mínimo 6 caractéres");
+  else setSuccess(passwordInput);
+
+  if (emailInput.value && passwordInput.value.length >= 6) login(data);
+});
 
 async function login(data) {
   try {
-    const response = await api.post('/users/login', data)
+    const response = await api.post("/users/login", data);
 
     if (response.status === 200) {
-      const userData = response.data
+      const userData = response.data;
 
-      localStorage.setItem('userId', userData.userId)
-      location.href = "listar-recados.html"
+      localStorage.setItem("userId", userData.userId);
+      location.href = "listar-recados.html";
     }
   } catch (error) {
-    console.log('Erro ao fazer login', error)
+    console.log("Erro ao fazer login", error);
   }
 }
 
+function setError(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.textContent = message;
+  formControl.classList.remove("success");
+  formControl.classList.add("error");
+}
+
+function setSuccess(input) {
+  const formControl = input.parentElement;
+
+  formControl.classList.remove("error");
+  formControl.classList.add("success");
+}
